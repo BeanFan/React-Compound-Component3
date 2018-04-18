3fbfc9d8608cd2bb0cf940be636106b5d9ba05fc
fanwanyong <38458515+fanwanyong@users.noreply.github.com>
2018-04-18 10:00:47 +0800

    commit exercice

--- src/App.js
+++ src/App.js
@@ -6,11 +6,36 @@ import FaForward from "react-icons/lib/fa/forward";
 import FaBackward from "react-icons/lib/fa/backward";
 
 class RadioGroup extends Component {
+  state = {
+    activeIndex: 2
+  };
+
+  constructor() {
+    super();
+  }
+
+  setActiveIndex = index => {
+    this.setState({ activeIndex: index });
+  };
+
+  componentDidMount() {
+    this.setState(this.state);
+  }
+
   render() {
+    const activeIndex = this.state.activeIndex;
+    const setActiveIndex = this.setActiveIndex;
+    let children = React.Children.map(this.props.children, (child, index) => {
+      return React.cloneElement(child, {
+        isActive: index === activeIndex,
+        onchangeButton: () => setActiveIndex(index)
+      });
+    });
+
     return (
       <fieldset className="radio-group">
         <legend>{this.props.legend}</legend>
-        {this.props.children}
+        {children}
       </fieldset>
     );
   }
@@ -18,9 +43,13 @@ class RadioGroup extends Component {
 
 class RadioButton extends Component {
   render() {
-    const isActive = false; // <-- should come from somewhere
+    const isActive = this.props.isActive; // <-- should come from somewhere
     const className = "radio-button " + (isActive ? "active" : "");
-    return <button className={className}>{this.props.children}</button>;
+    return (
+      <button onClick={this.props.onchangeButton} className={className}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
